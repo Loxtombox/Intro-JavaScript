0874e2ab74730906e8561e84625a45d895d2a182
Gerardo Teunissen <gerardo.teunissen@hotmail.com>
2022-09-15 17:12:00 +0100

    Primer commit, uso de variables

---
+++ app.js
@@ -0,0 +1,21 @@
+var nombre = 'Gerardo';
+console.log(nombre);
+console.log(typeof(nombre));
+
+var edad = 24;
+console.log(edad);
+console.log(typeof(edad));
+
+
+var sueldo = 1990.99;
+console.log(sueldo);
+console.log(typeof(sueldo));
+
+var tieneTrabajo = false;
+console.log(tieneTrabajo);
+console.log(typeof(tieneTrabajo));
+
+var puestoDeTrabajo;
+console.log(puestoDeTrabajo);
+puestoDeTrabajo=null;
+console.log(puestoDeTrabajo);

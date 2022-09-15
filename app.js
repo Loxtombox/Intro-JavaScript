e6a28f28f1f4fd01ecafa469a8187a9df589293f
Gerardo Teunissen <gerardo.teunissen@hotmail.com>
2022-09-15 23:40:18 +0100

    Operaciones matemáticas

--- app.js
+++ app.js
@@ -20,3 +20,23 @@ console.log(puestoDeTrabajo);
 puestoDeTrabajo=null;
 console.log(puestoDeTrabajo); */
 
+//Operadores matemáticos
+
+var edadAna, edadMaria, diferenciaEdad;
+var sumaEdades, yearAna, yearMaria, yearActual;
+
+edadAna = 34;
+edadMaria = 28;
+yearActual = 2022;
+
+diferenciaEdad = edadAna - edadMaria;
+sumaEdades = edadAna + edadMaria;
+yearAna = yearActual - edadAna;
+yearMaria = yearActual - edadMaria;
+
+console.log(diferenciaEdad);
+console.log(sumaEdades);
+console.log('Año en que nació Ana: ' + yearAna);
+console.log('Año en que nació María: ' + yearMaria);
+console.log(yearActual * 5);
+console.log(yearActual / 2);
